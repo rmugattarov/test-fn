a93aff4bc9853b968b11d0f6d1d1688df0cc4ee9
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-02-29 14:28:24 +0500

    updates

--- src/test/java/rmugattarov/Query_9711.java
+++ src/test/java/rmugattarov/Query_9711.java
@@ -1,10 +1,20 @@
 package rmugattarov;
 
-import com.filenet.api.core.*;
+import com.filenet.api.collection.RepositoryRowSet;
+import com.filenet.api.core.Connection;
+import com.filenet.api.core.Domain;
+import com.filenet.api.core.Factory;
+import com.filenet.api.core.ObjectStore;
+import com.filenet.api.property.Properties;
+import com.filenet.api.query.RepositoryRow;
+import com.filenet.api.query.SearchSQL;
+import com.filenet.api.query.SearchScope;
 import com.filenet.api.util.UserContext;
 import org.junit.Test;
 
 import javax.security.auth.Subject;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * Created by rmugattarov on 29.02.2016.
@@ -18,8 +28,33 @@ public class Query_9711 {
         try {
             Domain domain = Factory.Domain.fetchInstance(connection, null, null);
             ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, "OST", null);
-            Folder folder = Factory.Folder.fetchInstance(objectStore, "/Справочники/Объекты строительства", null);
-            System.out.println(folder.get_Id());
+            SearchScope searchScope = new SearchScope(objectStore);
+            Date startTime = new Date();
+            System.out.printf("\nStart time : %s\n\n", startTime);
+            SearchSQL searchSQL = new SearchSQL(
+                    "SELECT TOP 100 " +
+                            "d.Id,d.DateCreated,h.NewStatus " +
+                            "FROM (SzmnDocuments d " +
+                            "INNER JOIN DocumentsPack p " +
+                            "ON d.IdPack=p.IdPack)" +
+                            "INNER JOIN PackChangeHistory h " +
+                            "ON p.This=h.AnnotatedObject " +
+                            "WHERE " +
+                            "(d.IsCurrentVersion=TRUE) " +
+                            "AND (d.DocDate>=20160215T000000Z AND d.DocDate<=20160229T180000Z) " +
+                            "AND (h.PackStateChangeDate>=20160215T000000Z AND h.PackStateChangeDate<=20160229T180000Z)"
+            );
+            RepositoryRowSet repositoryRowSet = searchScope.fetchRows(searchSQL, null, null, true);
+            Iterator<RepositoryRow> iterator = repositoryRowSet.iterator();
+            int counter = 0;
+            while (iterator.hasNext()) {
+                RepositoryRow row = iterator.next();
+                Properties properties = row.getProperties();
+                System.out.printf("%d) Id : %s DateCreated : %s NewStatus : %s\n", ++counter, properties.getIdValue("Id"), properties.getDateTimeValue("DateCreated"), properties.getStringValue("NewStatus"));
+            }
+            Date finishTime = new Date();
+            System.out.printf("\nFinish time : %s\n", finishTime);
+            System.out.printf("Total time : %d s\n", (finishTime.getTime() - startTime.getTime()) / 1000);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
