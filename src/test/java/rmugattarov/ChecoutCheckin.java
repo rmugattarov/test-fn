6ef811291f281a1398fd4b17d247a51294743a4d
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-03-22 13:15:22 +0500

    updates

---
+++ src/test/java/rmugattarov/ChecoutCheckin.java
@@ -0,0 +1,48 @@
+package rmugattarov;
+
+import com.filenet.api.constants.AutoClassify;
+import com.filenet.api.constants.CheckinType;
+import com.filenet.api.constants.RefreshMode;
+import com.filenet.api.constants.ReservationType;
+import com.filenet.api.core.*;
+import com.filenet.api.util.Id;
+import com.filenet.api.util.UserContext;
+import org.junit.Test;
+
+import javax.security.auth.Subject;
+
+/**
+ * Created by rmugattarov on 22.03.2016.
+ */
+public class ChecoutCheckin {
+    @Test
+    public void test() {
+        Connection connection = Factory.Connection.getConnection("http://172.28.24.182:9080/wsi/FNCEWS40MTOM/");
+        Subject subject = UserContext.createSubject(connection, "rmugattarov@tn.fntst.ru", "o9p0[-]=", null);
+        UserContext.get().pushSubject(subject);
+        try {
+            Domain domain = Factory.Domain.fetchInstance(connection, null, null);
+            ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, "OST", null);
+            Document document = Factory.Document.fetchInstance(objectStore, new Id("{C0FD9C53-0000-C7FA-A521-975F88988842}"), null);
+            System.out.println(document.get_MajorVersionNumber());
+
+//            if (!document.get_IsCurrentVersion()) {
+//                System.out.println("Not current version");
+//                document = (Document) document.get_CurrentVersion();
+//            } else {
+//                System.out.println("Current version");
+//            }
+//
+//            document.checkout(ReservationType.EXCLUSIVE, null, null, null);
+//            document.save(RefreshMode.REFRESH);
+
+            document = (Document) document.get_Reservation();
+            document.checkin(AutoClassify.DO_NOT_AUTO_CLASSIFY, CheckinType.MAJOR_VERSION);
+            document.save(RefreshMode.REFRESH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            UserContext.get().popSubject();
+        }
+    }
+}
