df91a65e5359d648213384c12978a93f033e1b53
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-03-24 11:58:42 +0500

    updates

--- src/test/java/rmugattarov/AccessAllowed.java
+++ src/test/java/rmugattarov/AccessAllowed.java
@@ -20,7 +20,11 @@ public class AccessAllowed {
             Domain domain = Factory.Domain.fetchInstance(connection, null, null);
             ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, "OST", null);
             Document document = Factory.Document.fetchInstance(objectStore, "{8057A753-0000-CB1E-AD79-85656F7D61A8}", null);
+            System.out.println(document.getAccessAllowed());
             System.out.println(document.getAccessAllowed() & AccessRight.WRITE_AS_INT);
+            System.out.println(Integer.toBinaryString(document.getAccessAllowed()));
+            System.out.println(Integer.toBinaryString(2));
+            System.out.println(Integer.parseInt("00000000000000000010", 2));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
