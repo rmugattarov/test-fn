c41d77bbd007a1698a0dc7256183f85d04e50b7a
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-05-25 18:18:28 +0500

    updates

--- src/test/java/rmugattarov/TestObjectReference.java
+++ src/test/java/rmugattarov/TestObjectReference.java
@@ -21,11 +21,11 @@ public class TestObjectReference {
             Domain domain = Factory.Domain.fetchInstance(connection, null, null);
             ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, "OST", null);
             SearchScope searchScope = new SearchScope(objectStore);
-            Folder folder = Factory.Folder.fetchInstance(objectStore, "/test/rmugattarov/object-reference-test", null);
+            Folder folder = Factory.Folder.fetchInstance(objectStore, "/test/rmugattarov/test-object-reference", null);
             Document document = Factory.Document.createInstance(objectStore, "KatStroy", null);
             document.checkin(AutoClassify.DO_NOT_AUTO_CLASSIFY, CheckinType.MAJOR_VERSION);
-            Document parent = Factory.Document.fetchInstance(objectStore, "{804AE754-0000-CD5F-96CD-048DDFDB000C}", null);
-            document.getProperties().putObjectValue("ParentCatalogItem", parent.get_Id());
+            Document parent = Factory.Document.fetchInstance(objectStore, "{109CE754-0000-CD16-A27A-12C105284E3C}", null);
+            document.getProperties().putObjectValue("ParentCatalogItem", parent.getObjectReference());
             document.save(RefreshMode.NO_REFRESH);
             folder.file(document, AutoUniqueName.AUTO_UNIQUE, null, DefineSecurityParentage.DEFINE_SECURITY_PARENTAGE).save(RefreshMode.NO_REFRESH);
         } catch (Exception e) {
