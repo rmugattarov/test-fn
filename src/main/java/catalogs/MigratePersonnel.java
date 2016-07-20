1ba20951c65ec1702e02b0092b061b50f06b8e86
rmugattarov <rmugattarov@RMugattarov2-PC.datateh.ru>
2016-07-20 11:30:43 +0500

    updates

--- src/main/java/catalogs/MigratePersonnel.java
+++ src/main/java/catalogs/MigratePersonnel.java
@@ -4,9 +4,14 @@ import com.filenet.api.core.Connection;
 import com.filenet.api.core.Domain;
 import com.filenet.api.core.Factory;
 import com.filenet.api.core.ObjectStore;
+import com.filenet.api.property.Properties;
+import com.filenet.api.query.RepositoryRow;
+import com.filenet.api.query.SearchSQL;
+import com.filenet.api.query.SearchScope;
 import com.filenet.api.util.UserContext;
 
 import javax.security.auth.Subject;
+import java.util.Iterator;
 
 /**
  * Created by rmugattarov on 20.07.2016.
@@ -19,7 +24,21 @@ public class MigratePersonnel {
         try {
             Domain domain = Factory.Domain.fetchInstance(connection, null, null);
             ObjectStore objectStore = Factory.ObjectStore.fetchInstance(domain, "OST", null);
-            System.out.println(objectStore);
+            SearchScope searchScope = new SearchScope(objectStore);
+            SearchSQL searchSQL = new SearchSQL();
+            searchSQL.setFromClauseInitialValue("Personnel", null, true);
+            searchSQL.setSelectList("PersNumber,PersFio,IsCatalogElementActive");
+            searchSQL.setWhereClause("PersNumber IS NOT NULL AND PersFio IS NOT NULL");
+            Iterator<RepositoryRow> iterator = searchScope.fetchRows(searchSQL, null, null, true).iterator();
+            int counter = 0;
+            while (iterator.hasNext()) {
+                RepositoryRow row = iterator.next();
+                Properties properties = row.getProperties();
+                String persNumber = properties.getStringValue("PersNumber");
+                String persFio = properties.getStringValue("PersFio");
+                Boolean isCatalogElementActive = properties.getBooleanValue("IsCatalogElementActive");
+                System.out.printf("%d) %s %s %b\r\n", ++counter, persNumber, persFio, isCatalogElementActive);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
